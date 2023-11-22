94a431305e7731319b53d1fca6c92fd419cc3fb1
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2023-11-22 22:10:37 +0530

    dockerfile added

--- service/scrape.service.js
+++ service/scrape.service.js
@@ -1,9 +1,23 @@
 const puppeteer = require("puppeteer");
 const TrackModel = require("../model/TrackModel");
+require("dotenv").config();
 
 async function scrape(details) {
   console.log("Scraping started");
-  const browser = await puppeteer.launch({ headless: "new" });
+  let browser;
+  if (process.env.PUPPETEER_EXECUTABLE_PATH) {
+    browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    });
+  } else {
+    browser = await puppeteer.launch({ headless: "new" });
+  }
   const page = await browser.newPage();
   for (const detail of details) {
     await findPrice(page, detail);
